09cc6b4bf7fdb976c3e35b83d30949c3c6fc9b0c
Kevin Hoercher <khoerch@gmail.com>
2019-07-31 11:26:17 -0400

    Add interactive elements to project

--- src/App.js
+++ src/App.js
@@ -4,11 +4,38 @@ import SearchBar from './SearchBar/SearchBar';
 import FilterableList from './FilterableList/FilterableList';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			searchTerm: '',
+			filterOption: 'All'
+		}
+	}
+
+	updateSearchTerm(term) {
+		this.setState({
+			searchTerm: term
+		})
+	}
+
+	updateFilterOption(filter) {
+		this.setState({
+			filterOption: filter
+		})
+	}
+
 	render() {
 		return(
 			<div className="App">
-				<SearchBar />
-				<FilterableList files={this.props.files} />
+				<SearchBar 
+					searchTerm={this.state.searchTerm}
+					filterOption={this.state.filterOption}
+					handleUpdate={term => this.updateSearchTerm(term)}
+					handleFilter={filter => this.updateFilterOption(filter)}/>
+				<FilterableList 
+					files={this.props.files} 
+					searchTerm={this.state.searchTerm} 
+					filterOption={this.state.filterOption}/>
 			</div>
 		)
 	}
